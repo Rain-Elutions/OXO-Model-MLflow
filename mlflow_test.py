71ee2a21c66359a2648fe0c0a2de92c55b42133b
Zhuoyu Feng <zfeng0401@elutions.com>
2023-04-28 11:08:57 -0400

    fix log_model() bug

--- mlflow_test.py
+++ mlflow_test.py
@@ -5,8 +5,6 @@ import sys
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import ElasticNet
 from sklearn.preprocessing import MinMaxScaler
 from urllib.parse import urlparse
 import mlflow
